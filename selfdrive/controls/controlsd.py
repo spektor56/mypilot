8d07ce6763eeb8ce8f59cce15dc3b7b8c8742617
Brett Sanderson <l-brett-l@hotmail.com>
2022-05-10 02:18:26 -0400

    remove unused reference

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -28,7 +28,7 @@ from selfdrive.controls.lib.events import Events, ET
 from selfdrive.controls.lib.alertmanager import AlertManager, set_offroad_alert
 from selfdrive.controls.lib.vehicle_model import VehicleModel
 from selfdrive.locationd.calibrationd import Calibration
-from system.hardware import HARDWARE, TICI, EON
+from system.hardware import HARDWARE, TICI
 from selfdrive.manager.process_config import managed_processes
 
 SOFT_DISABLE_TIME = 3  # seconds
