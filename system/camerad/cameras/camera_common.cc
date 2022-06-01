33e2f2ef048a7ec007ff72d75c0f7965df1e7702
Brett Sanderson <l-brett-l@hotmail.com>
2022-06-01 17:46:53 -0400

    add back params

--- system/camerad/cameras/camera_common.cc
+++ system/camerad/cameras/camera_common.cc
@@ -13,6 +13,7 @@
 #include "system/camerad/imgproc/utils.h"
 #include "common/clutil.h"
 #include "common/modeldata.h"
+#include "common/params.h"
 #include "common/swaglog.h"
 #include "common/util.h"
 #include "system/hardware/hw.h"
