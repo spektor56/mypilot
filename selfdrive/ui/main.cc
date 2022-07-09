136664f3011a445077ee332c7398e25c131ba268
Shane Smiskol <shane@smiskol.com>
2022-07-08 20:25:54 -0700

    UI: Internationalization support (#21212)
    * rough multiple language demo
    
    * more wrappings
    
    * stash
    
    * add some bad translations
    
    * updates
    
    * map from french to spanish still has same problem of needing to call setText on everything
    
    * add files
    
    * restart UI
    
    * use return code
    
    * relative path
    
    * more translations
    
    * don't loop restart
    
    * Toggle and prime translations
    
    * try on device
    
    * try QComboBox with readable style
    
    * stash
    
    * not yet scrollable
    
    * stash
    
    * dynamic translations (doesn't work for dynamic widget strings yet)
    
    * clean up multiple option selector
    
    * store languages in json
    
    * try transparent
    
    * Try transparent popup
    
    * see how this looks
    
    * tweaks
    
    * clean up
    
    * update names
    
    * Add Chinese (Simplified) translations
    
    * Do missing French translations
    
    * unit tests caught that :)
    
    * fix test
    
    * fix other test (on PC)
    
    * add entries to dialog to test
    
    * add cancel button, clean up a bit
    
    * just chinese
    
    * some clean up
    
    * use quotes
    
    * clean up
    
    * Just quit, set timeout to 0
    
    * half a second
    
    * use exitcode
    
    * don't print if it's expected
    
    * this comment is outdated
    
    * update translations
    
    * Update translations
    
    * re-order input classes
    
    * Update line numbers
    
    * use enabled property for button style
    
    * Get rid of ListWidget
    
    * Update line numbers
    
    * Log failed to load language
    
    * Log failed to load language
    
    * Move to utils and fix english logging
    
    extra line
    
    * Update translations
    
    * spacing
    
    * looks a bit better
    
    * try this instead of exitcode
    
    fixes
    
    fix
    
    * only one function
    
    * comment
    
    * Update line numbers
    
    * fixup some japanese translations
    
    * clean up multi option dialog
    
    * Update line numbers

--- selfdrive/ui/main.cc
+++ selfdrive/ui/main.cc
@@ -2,6 +2,7 @@
 
 #include <QApplication>
 #include <QSslConfiguration>
+#include <QTranslator>
 
 #include "system/hardware/hw.h"
 #include "selfdrive/ui/qt/qt_window.h"
@@ -14,6 +15,15 @@ int main(int argc, char *argv[]) {
   qInstallMessageHandler(swagLogMessageHandler);
   initApp(argc, argv);
 
+  QTranslator translator;
+  QString translation_file = QString::fromStdString(Params().get("LanguageSetting"));
+  if (!translator.load(translation_file, "translations") && translation_file.length()) {
+    qCritical() << "Failed to load translation file:" << translation_file;
+  }
+
+  QApplication a(argc, argv);
+  a.installTranslator(&translator);
+
   if (Hardware::EON()) {
     QSslConfiguration ssl = QSslConfiguration::defaultConfiguration();
     ssl.setCaCertificates(QSslCertificate::fromPath("/usr/etc/tls/cert.pem"));
@@ -21,6 +31,8 @@ int main(int argc, char *argv[]) {
   }
 
   QApplication a(argc, argv);
+  a.installTranslator(&translator);
+
   MainWindow w;
   setMainWindow(&w);
   a.installEventFilter(&w);
